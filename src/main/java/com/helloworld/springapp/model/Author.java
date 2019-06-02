635c3953bb6b1e66e679658ddbcb825af2e1daca
Victor <victor.herrera.sc@itszapopan.edu.mx>
2019-06-02 08:40:42 -0500

    Added hashCode, toString and equals methods to Author and Book

--- src/main/java/com/helloworld/springapp/model/Author.java
+++ src/main/java/com/helloworld/springapp/model/Author.java
@@ -3,6 +3,7 @@ package com.helloworld.springapp.model;
 import javax.annotation.Generated;
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -63,5 +64,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
